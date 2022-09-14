5024949bd87d1e883730537f3221ccf483abe843
Amzsergio <ac.sergioandres@gmail.com>
2022-09-13 22:39:30 -0500

    first post for users module

--- api/src/index.js
+++ api/src/index.js
@@ -7,6 +7,25 @@ const server = express();
 server.use(express.json());
 server.use(morgan('dev'));
 
-// server.post('/users', async (req, res) => {
-//     const {}
-// })
+server.post('/users', async (req, res) => {
+    const {name, email, password, phone, address, type_document, number_document} = req.body;
+    try {
+        const newUser = await user.create({
+            name,
+            email,
+            password,
+            phone,
+            address,
+            type_document,
+            number_document
+        });
+        res.status(200).json(newUser)
+    } catch (error) {
+        res.status(400).send(`Error:${error}`)
+    }
+})
+
+server.listen(3001, ()=>{
+    console.log("Server running on port 3001");
+    db.sync({force: true});
+})
