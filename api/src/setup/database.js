efa59e55d2b418c766fe8c2725733048586c2780
Amzsergio <ac.sergioandres@gmail.com>
2022-09-13 22:13:33 -0500

    setting up DataBase

--- api/src/setup/database.js
+++ api/src/setup/database.js
@@ -1,10 +1,26 @@
-//imports
-const { Sequelize } = require('sequelize');
+const { Sequelize, Op } = require('sequelize');
 
-//create new database connection
+//import models
+const userModel = require('../modules/user/model.js');
+const roleModel = require('../modules/role/model.js');
+const orderModel = require('../modules/order/model.js');
+const productModel = require('../modules/product/model.js');
+const categoriesModel = require('../modules/categories/model.js');
+
 const connection = new Sequelize('postgres://postgres:admin123@localhost:5432/frutapp', {
     logging: false
 })
 
+userModel(connection);
+roleModel(connection);
+orderModel(connection);
+productModel(connection);
+categoriesModel(connection);
 
-exports.connection = connection;
+
+
+module.exports = {
+    ...connection.models,
+    db: connection,
+    Op
+}
